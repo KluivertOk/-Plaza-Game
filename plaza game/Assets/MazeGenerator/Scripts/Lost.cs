1d94aee6f8d8935e2d23a225e4fb46d66c1701cd
KluivertOk <Kluivertokobi@yahoo.com>
2022-08-06 21:06:25 +0100

    Fixing things

--- plaza game/Assets/MazeGenerator/Scripts/Lost.cs
+++ plaza game/Assets/MazeGenerator/Scripts/Lost.cs
@@ -1,18 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Lost : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void StartExit3()
     {
-        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 3);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
